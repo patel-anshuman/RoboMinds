f1027398a4d3030083ceab413b4a6ac39893758c
Abhishek <mistryabhishek2000@gmail.com>
2023-07-28 15:20:03 +0530

    Basic Frontend Structure Added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <p className='text-xl font-semibold text-blue-700'>My App</p> */}
+    </div>
+  );
+}
+
+export default App;
