73450230387235902f687dc855acfd460feac3f8
Anshuman Patel <patelanshuman6@gmail.com>
2023-07-29 23:09:38 +0530

    deleted stream

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -209,8 +209,7 @@ const HomePage: React.FC = () => {
         },
         body: JSON.stringify({
           model: "gpt-3.5-turbo",
-          messages: [{ role: "user", content: message }],
-          stream: true,
+          messages: [{ role: "user", content: message }]
         }),
       });
 
@@ -219,34 +218,6 @@ const HomePage: React.FC = () => {
       setInterviewerResponse(data.choices[0].message.content);
       setUserAnswer("");
 
-      // Read the response as a stream of data
-      const reader = response.body.getReader();
-      const decoder = new TextDecoder("utf-8");
-      setInterviewerResponse("");
-
-      while (true) {
-        const { done, value } = await reader.read();
-        if (done) {
-          break;
-        }
-        // Massage and parse the chunk of data
-        const chunk = decoder.decode(value);
-        const lines = chunk.split("\\n");
-        const parsedLines = lines
-          .map((line) => line.replace(/^data: /, "").trim()) // Remove the "data: " prefix
-          .filter((line) => line !== "" && line !== "[DONE]") // Remove empty lines and "[DONE]"
-          .map((line) => JSON.parse(line)); // Parse the JSON string
-
-        for (const parsedLine of parsedLines) {
-          const { choices } = parsedLine;
-          const { delta } = choices[0];
-          const { content } = delta;
-          // Update the UI with the new content
-          if (content) {
-            setInterviewerResponse(interviewerResponse + content);
-          }
-        }
-      }
     } catch (error) {
       console.error("Error:", error);
       setInterviewerResponse("Error occurred while generating.");
